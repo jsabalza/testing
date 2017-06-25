82caf46146d52d990a40603c5ba64ad4fb6cb969
Juan <ji.sabalza@gmail.com>
2017-06-25 14:56:39 -0300

    Added test case.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MDL } from './directives/material-element.directive'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MDL
   ],
   imports: [
     BrowserModule
